f131be560679a4508e00894ec8856ea9d5b9ebe6
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-09-26 18:47:16 +0300

    Solve e2e tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./server/app')
-const logger = require('./server//utils/logger')
+//const logger = require('./server//utils/logger')
 const config = require('./server//utils/config')
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  console.log(`Server running on port ${config.PORT}`)
 })
