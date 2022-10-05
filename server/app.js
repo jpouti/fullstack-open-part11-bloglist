1f96e04a0ebd6096bd8d9a061ec89ed3881fca5b
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-10-05 14:37:10 +0300

    Add heroku deployment for pipeline & health endpoint for app

--- server/app.js
+++ server/app.js
@@ -30,6 +30,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+// create endpoint for healthcheck
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
